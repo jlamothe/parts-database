5a5f84deb90f9a5cb6be24dfa14ec2d601c666ef
Jonathan Lamothe <jonathan@jlamothe.net>
2015-03-09 11:12:35 -0400

    Create Admin Users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+  let(:ability) { Ability.new(subject) }
+
+  context 'user not signed in' do
+    it 'should not be able to manage the user' do
+      expect(ability.can? :manage, User).to eq false
+    end
+  end
+
+  context 'user signed in' do
+    context 'as non-admin' do
+      before do
+        subject.save!
+      end
+
+      it 'should not be able to manage users' do
+        expect(ability.can? :manage, User).to eq false
+      end
+    end
+
+    context 'as admin' do
+      before do
+        subject.admin = true
+        subject.save!
+      end
+
+      it 'should be able to manage users' do
+        expect(ability.can? :manage, User).to eq true
+      end
+    end
+  end
 end
